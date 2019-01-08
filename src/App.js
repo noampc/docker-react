fffbe6e3ee7084503a1a7175ae848886240c96f9
noampc <noam.goldface@gmail.com>
2019-01-08 15:19:42 +0200

    fix text

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            tal ijjkj
+            noam goldface
           </a>
         </header>
       </div>
